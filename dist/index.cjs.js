f9c5cca2916cb3bb44fa80210edddbcba38d143b
CrashBandiCoin <88516022+adamChafqani@users.noreply.github.com>
2021-09-27 03:30:31 +0200

    initial commit

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -2285,11 +2285,6 @@ var socials = [
         icon: "TwitterIcon",
         href: "https://twitter.com/goosedefi",
     },
-    {
-        label: "Reddit",
-        icon: "RedditIcon",
-        href: "https://www.reddit.com/r/GooseFinanceofficial",
-    },
 ];
 var MENU_HEIGHT = 64;
 var MENU_ENTRY_HEIGHT = 48;
